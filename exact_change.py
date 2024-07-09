3a12051778dd8fd0d18ef9d3f3d1ee227cdc3c58
retsu8 <teck.geek1991@gmail.com>
2024-07-08 22:53:46 -0600

    Fixing spaces and adding dollars

--- exact_change.py
+++ exact_change.py
@@ -2,12 +2,26 @@ import sys
 
 change = int(input())
 
+if change == 0:
+    print("No change")
+    sys.exit()
+
+# Handle Dollars
+dollars = int(change / 100)
+if dollars > 1:
+    print(f"""{dollars} Dollars""")
+elif dollars == 1:
+    print(f"""{dollars} Dollar""")
+
+if dollars:
+    change = change - 100 * dollars
+
 # Handle the quearters
 quarters = int(change / 25)
 if quarters > 1:
     print(f"""{quarters} Quarters""")
 elif quarters == 1:
-    print(f"""{quarters} Quarter """)
+    print(f"""{quarters} Quarter""")
 
 if quarters:
     change = change - 25 * quarters
@@ -18,7 +32,7 @@ dimes = int(change / 10)
 if dimes > 1:
     print(f"""{dimes} Dimes""")
 elif dimes == 1:
-    print(f"""{dimes} Dime """)
+    print(f"""{dimes} Dime""")
 
 if dimes:
     change = change - 10 * dimes
@@ -28,7 +42,7 @@ nickels = int(change / 5)
 if nickels > 1:
     print(f"""{nickels} Nickels""")
 elif nickels == 1:
-    print(f"""{nickels} Nickel """)
+    print(f"""{nickels} Nickel""")
 
 if nickels:
     change = change - 5 * nickels
@@ -37,4 +51,4 @@ if nickels:
 if change > 1:
     print(f"""{change} Pennies""")
 elif change == 1:
-    print(f"""{change} Penny """)
+    print(f"""{change} Penny""")
