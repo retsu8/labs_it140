d8b7c7e7a49afdbb3b945341f77546f7f5b9019e
retsu8 <teck.geek1991@gmail.com>
2024-08-06 00:55:12 -0600

    cleaning script

--- milestone_6_4/ModuleSixMilestone.py
+++ milestone_6_4/ModuleSixMilestone.py
@@ -6,24 +6,28 @@
 # A dictionary for the simplified dragon text game
 # The dictionary links a room to other rooms.
 
+import sys
 
 class Player:
-    "Basic player class to define play actions"
+    """Basic player class to define play actions"""
 
     def __init__(self):
-        "Initialize the player"
+        """Initialize the player"""
         self.location = "Great Hall"
 
     def update_location(self, loc):
-        "Updat the player location"
+        """Updat the player location"""
         self.location = loc
 
     def get_location(self):
+        """Return player location"""
         return self.location
 
 
 class Game:
+    """Basic Game class"""
     def __init__(self):
+        """Setup gameplay basics, build player, set rooms"""
         self.player = Player()
         self.rooms = {
             "Great Hall": {"South": "Bedroom"},
@@ -31,18 +35,22 @@ class Game:
             "Cellar": {"West": "Bedroom"},
         }
 
+        # Make sure to welcome player
         print("Welcome to dungean")
         print("There are five commands here to use; exit, North, South, East, and West")
 
     def main(self):
+        """Working main class for game"""
         while self.player.get_location() != "exit":
             print(f"You are in {self.player.location}; where would you like to go?")
-            movement = input()
+            movement = input().capitalize()
             if movement in self.rooms[self.player.location]:
                 self.player.update_location(self.rooms[self.player.location][movement])
             elif movement == "exit":
                 self.player.update_location("exit")
-
+                sys.exit()
+            else:
+                print("Theres no door there?")
 
 if __name__ == "__main__":
     game = Game()
