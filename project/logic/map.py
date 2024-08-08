73f3340776685e97c6d77c2299c6c00f8b456d7f
retsu8 <teck.geek1991@gmail.com>
2024-08-07 22:17:45 -0600

    adding in get input

--- project/logic/map.py
+++ project/logic/map.py
@@ -10,49 +10,49 @@ class Map:
                 "description": "Summoning room for Cthulahu",
                 "item": "",
                 "room_image": "",
-                "connected_rooms": ["lab"]
+                "connected_rooms": {"west":"lab"}
             },
             "lab": {
                 "name": "Lab",
                 "description": "Room for resaearch",
                 "item": "green_slime",
                 "room_image": "",
-                "connected_rooms": ["meeting_room", "start_room", "kitchen"] 
+                "connected_rooms": {"west":"meeting_room", "east":"start_room", "south":"kitchen"} 
             },
             "meeting_room": {
                 "name": "Meeting Room",
                 "description": "Room with lots of couches and a few armchairs.",
                 "item": "research_notes",
                 "room_image": "",
-                "connected_rooms": ["lab", "kiras_room", "bruces_room", "koals_room"] 
+                "connected_rooms": {"east":"lab", "north":"kiras_room", "west":"bruces_room", "south":"koals_room"}
             },
             "kiras_room": {
                 "name": "Kira's Room",
                 "description": "Kira's the lab assistants room. Nice and neat jsut like she was.",
                 "item": "secret_notes",
                 "room_image": "",
-                "connected_rooms": ["meeting_room"] 
+                "connected_rooms": {"south":"meeting_room"}
             },
             "bruces_room": {         
                 "name": "Bruce's Room",       
                 "description": "Bruce the lab technician. A very empty room, with almost nothing in it.",
                 "item": "lab_id",
                 "room_image": "",
-                "connected_rooms": ["meeting_room"]
+                "connected_rooms": {"east":"meeting_room"}
             },
             "koals_room": {
                 "name": "Koal's Room",
                 "description": "Koal's the lab lead.  Always piled with clutter.",
                 "item": "acid",
                 "room_image": "",
-                "connected_rooms": ["meeting_room"]
+                "connected_rooms": {"south":"meeting_room"}
             },
             "staff_quarters": {
                 "name": "Staff Quarters",
                 "description": "Hy! Its the old staff quarters, always feels like home",
                 "item": "sandwich",
                 "room_image": "",
-                "connected_rooms": ["kitchen", "armory", "garage"]
+                "connected_rooms": {"north":"kitchen", "west":"armory", "east":"garage"}
             },
             "lounge": {
                 "name": "Lounge",
@@ -64,7 +64,7 @@ class Map:
                 "description": "Lots of old crates and boxes in here, laboled with everything the research team needed.",
                 "item": "handgun",
                 "room_image": "",
-                "connected_rooms": ["staff_quarters"],
+                "connected_rooms": {"east":"staff_quarters"},
                 "locked": "lab_id"
             },
             "kitchen": {
@@ -72,14 +72,14 @@ class Map:
                 "description": "Always loved to work in the kitchen, its the closest to the food.",
                 "item": "vehicle_keys",
                 "room_image": "",
-                "connected_rooms": ["lab", "lounge", "staff_quarters"]
+                "connected_rooms": {"north":"lab", "east":"lounge", "south":"staff_quarters"}
             },
             "garage": {
                 "name": "Garage",
                 "description": "There an old jeap in here, wounder if it actually runs.",
                 "item": "crystals",
                 "room_image": "",
-                "connected_rooms": ["staff_quarters"],
+                "connected_rooms": {"east":"staff_quarters"},
                 "locked": "acid"
             }
         }
--- project/main.py
+++ project/main.py
@@ -20,9 +20,9 @@ class Game:
             print(self.dialogue.get_rooms_promt(self.player.get_location()))
         else:
             print(self.dialogue.get_room_promt(self.player.get_location()))
-        for room in self.maps.get_connected_rooms(self.player.get_location()):
+        for key, room in self.maps.get_connected_rooms(self.player.get_location()).items():
             room = self.maps.get_name(room)
-            print(room)
+            print(f'{key}: {room}')
         selection = input()
         re.sub("[^A-Za-z0-9 ]+", "", selection)
         selection = selection.lower().replace(" ", "_")
@@ -61,10 +61,13 @@ class Game:
         if room_item not in self.player.get_items():
             pickup = input(self.dialogue.pickup_item(room_item["name"]))
             pickup = pickup.split(" ")
-            if "get" in pickup and room_item["Name"] in pickup[1:]:
-                self.player.pickup_item(room_item)
-                print(self.dialogue.pickedup_item(room_item["name"]))
-                print(self.dialogue.get_item_description(room_item["description"]))
+            print(pickup)
+            if "get" in pickup:
+                if room_item["name"].join("_") in pickup:
+                    pickup.remove("get")
+                    self.player.pickup_item(room_item)
+                    print(self.dialogue.pickedup_item(room_item["name"]))
+                    print(self.dialogue.get_item_description(room_item["description"]))
             else:
                 print(self.dialogue.leave_item())
 
@@ -79,12 +82,13 @@ class Game:
             print(self.dialogue.get_room_description(description))
             print(self.dialogue.get_player_inventory(self.player.get_inventory()))
             player_input = self.player_input_selection()
+            connected_rooms = self.maps.get_connected_rooms(self.player.get_location())
             if player_input == "0":
                 print(self.dialogue.get_player_died())
                 sys.exit()
             elif player_input == "help":
                 print(self.dialogue.get_additional_input())
-            elif player_input in self.maps.get_connected_rooms(self.player.get_location()):
+            elif player_input in connected_rooms.keys():
                 if player_input in villian:
                     if self.player.get_inventory_count() == self.items.get_count():
                         print("You Win")
@@ -92,12 +96,13 @@ class Game:
                         print(self.dialogue.get_player_meets_villan_unarmed())
                         self.handle_player_died()
                 else:
-                    key_item = self.check_locked_room(self.maps.location[player_input])
+                    key_location = connected_rooms[player_input]
+                    key_item = self.check_locked_room(self.maps.location[key_location])
                     if key_item in self.player.get_items() or not key_item:
-                        self.player.update_location(player_input)
+                        self.player.update_location(key_location)
                         self.handle_item_pickup(self.player.get_location())
                     elif key_item:
-                        print(self.dialogue.get_room_locked(player_input))
+                        print(self.dialogue.get_room_locked(key_location))
             else:
                 print(self.dialogue.get_invalid_input())
                 print(self.dialogue.get_rooms_promt(self.player.get_location()))
