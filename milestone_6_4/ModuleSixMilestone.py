84d8b91ed97e519d806b696d205a09e297b6ac24
retsu8 <teck.geek1991@gmail.com>
2024-08-07 21:11:03 -0600

    Added go find methods

--- milestone_6_4/ModuleSixMilestone.py
+++ milestone_6_4/ModuleSixMilestone.py
@@ -3,17 +3,15 @@
 # Module 6 Milestone: Moving Between Rooms
 #########################
 
-# A dictionary for the simplified dragon text game
-# The dictionary links a room to other rooms.
-
 import sys
+
 
 class Player:
     """Basic player class to define play actions"""
 
     def __init__(self):
         """Initialize the player"""
-        self.location = "Great Hall"
+        self.location = "Start Room"
 
     def update_location(self, loc):
         """Updat the player location"""
@@ -26,9 +24,10 @@ class Player:
 
 class Game:
     """Basic Game class"""
+
     def __init__(self):
-        """Setup gameplay basics, build player, set rooms"""
-        self.player = Player()
+        """Setup gameplay basics, build player, set rooms
+        Create basic rooms"""
         self.rooms = {
             "Great Hall": {"South": "Bedroom"},
             "Bedroom": {"North": "Great Hall", "East": "Cellar"},
@@ -37,21 +36,73 @@ class Game:
 
         # Make sure to welcome player
         print("Welcome to dungean")
-        print("There are five commands here to use; exit, North, South, East, and West")
+        print(
+            "There are five commands here to use; exit, or go [North, South, East, and West]\n\n"
+        )
 
-    def main(self):
-        """Working main class for game"""
-        while self.player.get_location() != "exit":
-            print(f"You are in {self.player.location}; where would you like to go?")
-            movement = input().capitalize()
-            if movement in self.rooms[self.player.location]:
-                self.player.update_location(self.rooms[self.player.location][movement])
-            elif movement == "exit":
-                self.player.update_location("exit")
-                sys.exit()
+    def validate_input(self, move):
+        """Validate input for the go move command"""
+        move = move.split(" ")
+
+        # Check to make sure we have a go command to move with
+        if "go" in move:
+            move.remove("go")
+            # Then find the direction intended
+            for word in move:
+                word = word.capitalize()
+                if word in ["North", "South", "East", "West"]:
+                    # Return that direction
+                    return word
+        
+        elif "exit" in move:
+            # Check for exit
+            return "exit"
+
+        # If go command not found and/or direction not used, fail input.
+        return False
+
+    def run_end_game(self):
+        """Quite the game function"""
+        while True:
+            """Loop for an answer"""
+            x = input("Would you like to quite the game? Y/N")    
+            if "y" in x.lower():
+                print("Thank you for playing!")
+                return True
+            elif "n" in x.lower():
+                print("Onward!")
+                return False
             else:
-                print("Theres no door there?")
+                print("Invalid input?")
+
+    def main(self, player):
+        """Working main class for game"""
+        player.update_location("Great Hall")
+        while player.get_location() != "exit":
+            # Create input loop for game commands
+            print(f"You are in {player.location}; where would you like to go?\n")
+            movement = self.validate_input(input())
+            # Validate the input loop
+            if movement in self.rooms[player.location]:
+                print(movement)
+                # Update the player location on valid input
+                player.update_location(self.rooms[player.location][movement])
+            elif movement == "exit":
+                # End game on exit
+                loc = player.get_location()
+                player.update_location("exit")
+                if self.run_end_game():
+                    continue
+                player.update_location(loc)
+            else:
+                # Junk input recieved dump and try again.
+                print("You cant go that way.\n")
+
 
 if __name__ == "__main__":
+    # Build the game
     game = Game()
-    game.main()
+
+    # Put the player in the game
+    player = Player()
+    game.main(player)
